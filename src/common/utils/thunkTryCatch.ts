0a25439710df38021e7da89cd721234c65adbf59
Sergey <santerakis@gmail.com>
2023-07-07 22:03:59 +0300

    jsdoc

--- src/common/utils/thunkTryCatch.ts
+++ src/common/utils/thunkTryCatch.ts
@@ -1,6 +1,15 @@
 import { BaseThunkAPI } from "@reduxjs/toolkit/dist/createAsyncThunk";
 import { AppDispatch, RootState } from "app/store";
 
+/**
+ * Обертка для Redux-Thunk, которая позволяет перехватывать и обрабатывать ошибки в сайд-эффектах.
+ * @async
+ * @function thunkTryCatch
+ * @param {BaseThunkAPI<RootState, any, AppDispatch, unknown>} thunkAPI - объект, содержащий методы `dispatch`, `getState`, `extra`, `rejectWithValue` для использования внутри `logic`.
+ * @param {Function} logic - сайд-эффект, который необходимо выполнить в `try...catch` блоке.
+ * @param {boolean} [showGlobalError=true] - флаг, указывающий, нужно ли выводить глобальную ошибку.
+ * @returns {Promise<any>} результат выполнения `logic` в случае успешного выполнения, либо объект `{ e, showGlobalError }`, в случае возникновения ошибки.
+ */
 export const thunkTryCatch = async (
   thunkAPI: BaseThunkAPI<RootState, any, AppDispatch, unknown>,
   logic: Function,
